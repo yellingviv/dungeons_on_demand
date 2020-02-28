989adcdd1e14ab31ca248057be7d8b25173d2abd
vivienne <vpustell@slack-corp.com>
2020-02-27 19:44:46 -0800

    create initial game stats doc to resolve errors of it not existing

---
+++ static/js/gameStats.jsx
@@ -0,0 +1,27 @@
+class SummonMonsters extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reg_message: '',
+            username: '',
+            password: ''
+        }
+        this.gameStats = this.gameStats.bind(this);
+    }
+
+    gameStats() {
+      return (
+        <div>
+            This is a placeholder.
+        </div>
+      )
+    }
+
+    render() {
+        return (
+            <div className="access_flow">
+                {this.gameStats()}
+            </div>
+        );
+    }
+}
