4e6811f00da5caee8c2dad872df991faeeca8486
vivienne <vpustell@slack-corp.com>
2020-03-03 20:15:54 -0800

    getting damage ready to happen omg yay

--- static/js/monsterList.jsx
+++ static/js/monsterList.jsx
@@ -3,14 +3,18 @@ class MonsterCard extends React.Component {
     super(props);
     this.state = {
             room_id: 10,
-            initiative: {}
+            initiative: {},
+            hp: {},
+            damage: {}
     }
     this.rollInit = this.rollInit.bind(this);
+    this.dealDamage = this.dealDamage.bind(this);
+    this.calculateDamage = this.calculateDamage.bind(this);
   }
 
     rollInit(evt) {
         const monster_id = evt.target.id;
-        console.log("target id: ", monster_id)
+        console.log("target id: ", monster_id);
         const init_url = '/roll_initiative?monster_id=' + monster_id;
         console.log("we called out this way: ", init_url);
         let response = fetch(init_url);
@@ -21,31 +25,53 @@ class MonsterCard extends React.Component {
         console.log("state is now: ", this.state);
     }
 
+    dealDamage(evt) {
+      const monster_id = evt.target.id;
+      console.log("dealDamage called, attempting to update html");
+      document.getElementById("damage_fields").innerHTML = "<form name='damage_call' onClick={this.calculateDamage}>How much damage? <input type='number' name='damage' onChange={this.damageHandling}/><br />Which player? <input type='text' name='player' /></form>";
+    }
+
+    calculateDamage(evt) {
+      evt.preventDefault();
+      const monster_id = evt.target.id;
+      const damage = this.state.damage[monster_id];
+      console.log("monster to hurt: ", monster_id);
+      console.log("damage to deal: ", damage);
+      const damage_url = "/roll_monster_damage?monster_id=" + monster_id + "&damage=" + damage;
+      let response = fetch(damage_url);
+      response.then((res) => res.json()).then((data) => {
+        console.log("damage done! ", data);
+        this.setState({hp: {[monster_id]: data}});
+      })
+    }
+
 	render() {
 		return (
 			<div className="monster">
 				<h2>Type: {this.props.type}</h2>
 				<h2 id="hp">HP: {this.props.hp} </h2>
-                <p>Initiative: {this.props.initiative_mod}<br />
-                AC: {this.props.ac}<br />
-                Hit dice: {this.props.dice_num}d{this.props.dice_type} + {this.props.bonus}</p>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>STR: {this.props.str}</td><td>DEX: {this.props.dex}</td><td>CON: {this.props.con}</td>
-                        </tr>
-                        <tr>
-                            <td>INT: {this.props.int}</td><td>WIS: {this.props.wis}</td><td>CHA: {this.props.cha}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <p>Speed: {this.props.speed} (Swim: {this.props.swim}, Fly: {this.props.fly}, Hover? {this.props.hover})<br />
-                Size: {this.props.size}
-                </p>
-                <p>
-                  Current Initiative: {this.state.initiative[this.props.monster_id]}<br />
-                  <button name="roll_init" type="button" id={this.props.monster_id} onClick={this.rollInit}>Roll Initiative</button>
-                </p>
+        <p>Initiative: {this.props.initiative_mod}<br />
+        AC: {this.props.ac}<br />
+        Hit dice: {this.props.dice_num}d{this.props.dice_type} + {this.props.bonus}</p>
+        <table>
+            <tbody>
+                <tr>
+                    <td>STR: {this.props.str}</td><td>DEX: {this.props.dex}</td><td>CON: {this.props.con}</td>
+                </tr>
+                <tr>
+                    <td>INT: {this.props.int}</td><td>WIS: {this.props.wis}</td><td>CHA: {this.props.cha}</td>
+                </tr>
+            </tbody>
+        </table>
+        <p>Speed: {this.props.speed} (Swim: {this.props.swim}, Fly: {this.props.fly}, Hover? {this.props.hover})<br />
+        Size: {this.props.size}
+        </p>
+        <p>
+          Current Initiative: {this.state.initiative[this.props.monster_id]} <button name="roll_init" type="button" id={this.props.monster_id} onClick={this.rollInit}>Roll Initiative</button><br />
+          Current HP: {this.state.hp[this.props.monster_id]} <button id={this.props.monster_id} onClick={this.dealDamage}>Deal Damage</button><br />
+        </p>
+        <p id="damage_fields">
+        </p>
 			</div>
 		);
 	}
