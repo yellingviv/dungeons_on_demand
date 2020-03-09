75e22d4204a79dc7a575cfbbaa8cda074d6b4cff
vivienne <viv@yellingviv.com>
2020-03-09 16:34:31 -0700

    adjusting the sort functions to return initiative in correct order

--- support_functions.py
+++ support_functions.py
@@ -6,11 +6,11 @@ def initiative_sort(init_order):
     """sorts all the characters for a given combat by initiative"""
 
     print("passed into sort function: ", init_order)
-    for i in range(len(init_order) - 1):
+    for i in range(len(init_order)):
         check = init_order[i]
         print("the check is: ", check, " and i is: ", i)
         index = i
-        while index > 0 and init_order[index - 1][0] > check[0]:
+        while index > 0 and init_order[index - 1][0] < check[0]:
             init_order[index] = init_order[index - 1]
             index = index - 1
         init_order[index] = check
